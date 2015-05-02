949b46a7270db701f894b3db021d4bb3a1e6fe37
Currix <currix@gmail.com>
2015-05-02 21:12:24 +0200

    Changed delta time treatment. Added bash script to build defined_functions.py.

--- python/check_time_interval.py
+++ python/check_time_interval.py
@@ -1,10 +1,11 @@
-def check_tinverval(tseries, interval=10):
+def check_tinverval(tseries, interval=10, iprint = 0):
     ##
     '''Return elements in the time series tseries that don't have the given frequency interval. The argument interval is given in units of minutes with a default value of 10 minutes.
 
        Arguments: 
-                  tseries :  time series
-                  interval:  time interval in minutes (default val = 10 min)
+             tseries  :  time series
+             interval :  time interval in minutes (optional, default val = 10 min)
+             iprint   :  verbosity control. (optional, default value = 0, min output)
 
        Output:
                   pandas series with data = irregular periods and index = times when irregular periods happen.
@@ -22,11 +23,17 @@ def check_tinverval(tseries, interval=10):
         ##
         delta = date_time - date_time_0
         ##
-        if (delta.seconds/60 != interval or delta.days != 0 ): ## Transforms seconds to mins
+        if (iprint > 0):
+            print date_time, date_time_0, delta
+        ##
+        if (delta/np.timedelta64(1, 'm') != interval ): ## numpy timedelta
+###        if (delta.seconds/60 != interval or delta.days != 0 ): ## Transforms seconds to mins
             ##
-            print "Irregular measure: ", date_time_0, date_time, delta
+###            print "Irregular measure: ", date_time_0, date_time, delta
+            print "Irregular measure: ", date_time_0, date_time, delta/np.timedelta64(1, 'm')
             irr_times.append(date_time_0)
-            irr_period.append(delta.seconds/60+delta.days*24*60)
+###            irr_period.append(delta.seconds/60+delta.days*24*60)
+            irr_period.append( delta/np.timedelta64(1, 'm') )
         ##
         date_time_0 = date_time
     ##
